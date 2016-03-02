15b62e1b50e31d83b4f9b17f99759840f37ddda6
Minsun Lee <dalzony@gmail.com>
2016-03-02 14:11:08 +0900

    2.1.3 오토와이어링 이용한 di

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,8 +1,6 @@
 package com.example;
 
-import com.example.app.Argument;
-import com.example.app.ArgumentResolver;
-import com.example.app.Calculator;
+import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -17,17 +15,8 @@ public class App
 {
     public static void main( String[] args ) {
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Enter 2 numbers like 'a b' :");
-
-            //int a = scanner.nextInt();
-            //int b = scanner.nextInt();
-
-            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
-            Argument argument = argumentResolver.resolve(System.in);
-            Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(argument.getA(),argument.getB());
-            System.out.println("Hello result = " + result);
+            Frontend frontend = context.getBean(Frontend.class);
+            frontend.run();
         }
 
     }
