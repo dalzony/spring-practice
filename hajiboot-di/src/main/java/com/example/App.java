9d71c053f76f78906660fa2dfce56a4a6efc1688
Minsun Lee <dalzony@gmail.com>
2016-03-02 11:35:06 +0900

    2.1.2 애플리케이션 추상황

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
 import com.example.app.Calculator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -17,12 +19,15 @@ public class App
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter 2 numbers like 'a b' :");
-            int a = scanner.nextInt();
-            int b = scanner.nextInt();
 
+            //int a = scanner.nextInt();
+            //int b = scanner.nextInt();
+
+            ArgumentResolver argumentResolver = context.getBean(ArgumentResolver.class);
+            Argument argument = argumentResolver.resolve(System.in);
             Calculator calculator = context.getBean(Calculator.class);
-            int result = calculator.calc(a,b);
-            System.out.println("result =" + result);
+            int result = calculator.calc(argument.getA(),argument.getB());
+            System.out.println("Hello result = " + result);
         }
 
     }
