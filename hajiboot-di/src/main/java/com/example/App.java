2b56501badb1a453404edd66527e70026cd13618
Minsun Lee <dalzony@gmail.com>
2016-03-02 14:36:46 +0900

    2.1.4 컴포넌트 스캔으로 Bean 등록

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -4,20 +4,16 @@ import com.example.app.Frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import java.util.Scanner;
 
 @EnableAutoConfiguration
-@Import(AppConfig.class)
-
-
-public class App 
-{
-    public static void main( String[] args ) {
+@ComponentScan
+public class App {
+    public static void main(String[] args) {
         try (ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
             Frontend frontend = context.getBean(Frontend.class);
             frontend.run();
         }
-
     }
 }
