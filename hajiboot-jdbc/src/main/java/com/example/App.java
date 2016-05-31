23bc9e37b3304d75fb022f3ed54bfd4ed1b38df4
Minsun Lee <dalzony@gmail.com>
2016-05-31 11:28:31 +0900

    2.2.1 JdbcTemplate 으로 DB접속하기 -1

--- hajiboot-jdbc/src/main/java/com/example/App.java
+++ hajiboot-jdbc/src/main/java/com/example/App.java
@@ -1,13 +1,39 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
+
 /**
  * Hello world!
  *
  */
-public class App 
+
+@EnableAutoConfiguration
+
+public class App implements CommandLineRunner
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    @Autowired
+    NamedParameterJdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public void run(String... strings) throws Exception {
+        String sql = "SELECT 1";
+        SqlParameterSource param = new MapSqlParameterSource();
+        Integer result = jdbcTemplate.queryForObject(sql, param, Integer.class);
+
+        System.out.println("result = " + result);
+
     }
+
+    public static void main( String[] args ) {
+        //System.out.println( "Hello World!" );
+        SpringApplication.run(App.class, args);
+    }
+
 }
